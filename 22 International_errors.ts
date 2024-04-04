771e167e6e48d8d76fd6eacdec7bb197c4d42266
ToobaRafiq <165496515+ToobaRafiq@users.noreply.github.com>
2024-04-04 07:48:50 -0700

    Add files via upload

---
+++ 22 International_errors.ts
@@ -0,0 +1,13 @@
+ 
+ // If you haven't recieved an array index error in one of your program yet, try to make one happen.
+
+ let newArray : string[] = ['Sajjal ali' ,'Kinza hashmi','Aiza khan'];
+
+ //Accessing array by index.
+
+ console.log(newArray[4]);
+
+ //Make sure you correct  the array before closing the program.
+ //Correct the Error.
+ console.log(newArray);
+
