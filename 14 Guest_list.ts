b3c8938f1c2bb340f7128eb3f5b637636bbea9d4
ToobaRafiq <165496515+ToobaRafiq@users.noreply.github.com>
2024-04-04 07:43:22 -0700

    Add files via upload

---
+++ 14 Guest_list.ts
@@ -0,0 +1,6 @@
+let Guest_list : string[] = ['Imran khan','Nawaz shareef', 'Asif ali zardari'];
+
+for(let i=0; i<Guest_list.length ;i++){
+  
+  console.log('Dear Mr. ' + Guest_list[i]  + ',\n\nIt is our pleasure to invite you in our party.\n\nThank You!')
+}
