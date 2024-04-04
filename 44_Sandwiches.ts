bcf5148c8a5d1163ec40f5aa74ccab0af787b14c
ToobaRafiq <165496515+ToobaRafiq@users.noreply.github.com>
2024-04-04 08:08:19 -0700

    Add files via upload

---
+++ 44_Sandwiches.ts
@@ -0,0 +1,15 @@
+// Write a function that accepts a array of items a person wants on a sandwich.
+//  The function should have one parameter that collects as many items as the function call provides,
+//   and it should print a summary of the sandwich that is being ordered.
+// Call the function three times, using a different number of arguments each time.
+
+function makesandwich (item: string[]){
+ console.log('\nMaking your sandwich with:');
+
+item.forEach(element => console.log("-" + element));
+console.log('Enjoy your sandwich !\n');
+}
+makesandwich(['Ham' ,'Cheese', 'lettuce']);
+makesandwich(['Turkey' , 'Bacon']);
+makesandwich(['Peanut butter','Jelly']);
+
