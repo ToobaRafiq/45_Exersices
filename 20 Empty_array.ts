5ccf4d6ed4aa3f82361eaa481029ed4e756eec08
ToobaRafiq <165496515+ToobaRafiq@users.noreply.github.com>
2024-04-04 07:47:14 -0700

    Add files via upload

---
+++ 20 Empty_array.ts
@@ -0,0 +1,12 @@
+ //Think of somethiing  you could store in a array.
+
+ let items :string[] = ['Pakistan','Karachi','Urdu'];
+
+ //Write a program that create a list containing these items.
+  let items2 : string[] = [];
+
+  items2.push('Germany');
+  items2.push('England');
+  items2.push('India');
+
+  console.log(items2);
