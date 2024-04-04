e797725740cf1a51f5643b631f63844640a29d71
ToobaRafiq <165496515+ToobaRafiq@users.noreply.github.com>
2024-04-04 07:40:36 -0700

    Add files via upload

---
+++ 12 Greeting.ts
@@ -0,0 +1,8 @@
+ 
+ let Names : string[] = ['Muniza', 'Tooba','Tahira'];
+
+const massage : string ='Welcome to typescript class:'
+ for(let i=0; i<Names.length; i++){
+
+    console.log( massage + Names [i]);
+ }
